f0dda724612f22078daec3019776f917a4304767
AlkemistDesign <paulamiguelpalencia@gmail.com>
2026-01-05 09:46:44 +0100

    rename repository

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base:"/Pokedexui",
+    base:"/PokedexUI",
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
